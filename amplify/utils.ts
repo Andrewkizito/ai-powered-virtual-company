2d0586df1a5528967d666220522528c0a65b36ac
andyclaudry <andrewkizito54@proton.me>
2026-07-07 13:27:40 +0300

    refactor: update AWS branch handling and clean up code

--- amplify/utils.ts
+++ amplify/utils.ts
@@ -1,15 +1,17 @@
-export const app_name = "virtual-company";
+export const app_name = "virtual-company"
 
-export const isSandbox = !process.env.AWS_BRANCH;
+export const isSandbox = !process.env.AWS_BRANCH
 
-export const envSuffix = isSandbox ? "-dev" : "";
+export const aws_branch = isSandbox ? "local" : process.env.AWS_BRANCH
 
-export const dynamodb_table_name = `${app_name}${envSuffix}`;
+export const envSuffix = aws_branch
 
-export const custom_group_resources_name = "Custom-Resources";
+export const dynamodb_table_name = `${app_name}${envSuffix}`
+
+export const custom_group_resources_name = "Custom-Resources"
 
 export const app_domain = isSandbox
   ? "http://localhost:5173"
-  : "https://main.d3tm84kkwwupqg.amplifyapp.com/";
+  : `https://${aws_branch}.${process.env.AWS_APP_ID}.amplifyapp.com/`
 
-export const auth_domain_prefix = `${app_name}${envSuffix}`;
+export const auth_domain_prefix = `${app_name}${envSuffix}`
