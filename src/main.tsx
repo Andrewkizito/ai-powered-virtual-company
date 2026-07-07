7054a7605daa3b03c47edccb08d7528689afb3ea
andyclaudry <andrewkizito54@proton.me>
2026-07-07 15:39:04 +0300

    feat: integrate AuthProvider and configure Amplify for authentication

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,20 @@ import { createRoot } from "react-dom/client"
 
 import "./index.css"
 import App from "./App.tsx"
+import { AuthProvider } from "react-oidc-context"
+import { Amplify } from "aws-amplify"
 import { ThemeProvider } from "@/components/theme-provider.tsx"
+import { cognitoAuthConfig, onSignIn } from "./lib/auth.ts"
+import outputs from "../amplify_outputs.json"
+
+Amplify.configure(outputs)
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
+    <AuthProvider {...cognitoAuthConfig} onSigninCallback={onSignIn}>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </AuthProvider>
   </StrictMode>
 )
