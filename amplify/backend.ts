3c23087d2363b88cf29c37eed478d54cd1e01661
andyclaudry <andrewkizito54@proton.me>
2026-07-07 15:15:07 +0300

    fix: add auth_domain_prefix import and output for Cognito auth domain

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,7 +2,7 @@ import { defineBackend } from "@aws-amplify/backend"
 import { auth, initAuth } from "./auth/resource"
 import { initDynamoDb, s3Storage } from "./storage/resource"
 import { postConfirmation } from "./functions/postConfirmation/resource"
-import { app_name, envSuffix } from "./utils"
+import { app_name, auth_domain_prefix, envSuffix } from "./utils"
 
 const backend = defineBackend({
   auth,
@@ -22,3 +22,9 @@ const dbTable = initDynamoDb(databaseStack)
 // Enforce bucket name to avoid auto generated names
 backend.storage.resources.cfnResources.cfnBucket.bucketName = `${app_name}-media-files${envSuffix}`
 dbTable.grantWriteData(backend.postConfirmation.resources.lambda)
+
+backend.addOutput({
+  custom: {
+    cognito_auth_domain: `https://${auth_domain_prefix}.auth.${process.env.AWS_REGION}.amazoncognito.com`,
+  },
+})
