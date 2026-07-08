e0948470b7fb339d10fc48403840cca08ca1e28b
andyclaudry <andrewkizito54@proton.me>
2026-07-08 14:29:56 +0300

    feat: add addInventory function definition for AWS Amplify

--- amplify/api/resource.ts
+++ amplify/api/resource.ts
@@ -2,12 +2,15 @@ import * as apigateway from "aws-cdk-lib/aws-apigateway"
 import { Construct } from "constructs"
 import { app_domain, app_name } from "../utils"
 import { IUserPool } from "aws-cdk-lib/aws-cognito"
+import { IFunction } from "aws-cdk-lib/aws-lambda"
 
 export const initRestApi = (params: {
   scope: Construct
   userPool: IUserPool
+  addInventoryLambda: IFunction
 }) => {
-  const restApi = new apigateway.RestApi(params.scope, "api", {
+  const { scope, userPool, addInventoryLambda } = params
+  const restApi = new apigateway.RestApi(scope, "api", {
     restApiName: `${app_name}`,
     description: "Rest api for this ecommerce site",
     defaultCorsPreflightOptions: {
@@ -17,18 +20,26 @@ export const initRestApi = (params: {
     },
   })
 
-  // const cognitoAuthorizer = new apigateway.CognitoUserPoolsAuthorizer(
-  //   restApi,
-  //   "cognitoAuthorizer",
-  //   {
-  //     cognitoUserPools: [params.userPool],
-  //     authorizerName: "cognito",
-  //     identitySource: apigateway.IdentitySource.header("Authorization"),
-  //   }
-  // )
+  const cognitoAuthorizer = new apigateway.CognitoUserPoolsAuthorizer(
+    restApi,
+    "cognitoAuthorizer",
+    {
+      cognitoUserPools: [userPool],
+      authorizerName: "cognito",
+      identitySource: apigateway.IdentitySource.header("Authorization"),
+    }
+  )
 
   const rootResource = restApi.root.addResource("inventory")
   rootResource.addMethod("GET", new apigateway.MockIntegration())
+  rootResource.addMethod(
+    "POST",
+    new apigateway.LambdaIntegration(addInventoryLambda),
+    {
+      authorizationType: apigateway.AuthorizationType.COGNITO,
+      authorizer: cognitoAuthorizer,
+    }
+  )
 
   return restApi
 }
