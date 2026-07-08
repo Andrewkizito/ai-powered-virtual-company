e1dcbd4dc475db38b244029b0e594692a00df8cd
andyclaudry <andrewkizito54@proton.me>
2026-07-08 14:45:54 +0300

    feat: integrate deleteInventory function into backend configuration

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -3,6 +3,7 @@ import { auth, initAuth } from "./auth/resource"
 import { initDynamoDb, s3Storage } from "./storage/resource"
 import { postConfirmation } from "./functions/postConfirmation/resource"
 import { addInventory } from "./functions/inventory/add-inventory/resource"
+import { deleteInventory } from "./functions/inventory/delete-inventory/resource"
 import { app_name, auth_domain_prefix, envSuffix } from "./utils"
 import { initRestApi } from "./api/resource"
 import { Stack } from "aws-cdk-lib"
@@ -12,6 +13,7 @@ const backend = defineBackend({
   storage: s3Storage,
   postConfirmation,
   addInventory,
+  deleteInventory,
 })
 
 initAuth({
@@ -27,7 +29,6 @@ const dbTable = initDynamoDb(databaseStack)
 // Enforce bucket name to avoid auto generated names
 backend.storage.resources.cfnResources.cfnBucket.bucketName = `${app_name}-media-files-${envSuffix}`
 dbTable.grantWriteData(backend.postConfirmation.resources.lambda)
-dbTable.grantWriteData(backend.addInventory.resources.lambda)
 
 // Rest API
 const restApiStack = backend.createStack("restApi")
@@ -35,7 +36,9 @@ const restApiStack = backend.createStack("restApi")
 const restApi = initRestApi({
   scope: restApiStack,
   userPool: backend.auth.resources.userPool,
+  dbTable,
   addInventoryLambda: backend.addInventory.resources.lambda,
+  deleteInventoryLambda: backend.deleteInventory.resources.lambda,
 })
 
 backend.addOutput({
