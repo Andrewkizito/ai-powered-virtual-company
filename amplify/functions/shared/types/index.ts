c1c0191ee91b059487528aeb960cc243793f3330
andyclaudry <andrewkizito54@proton.me>
2026-07-08 14:23:49 +0300

    feat: update exports in index.ts to include InventoryItemStatus and additional shopping types

--- amplify/functions/shared/types/index.ts
+++ amplify/functions/shared/types/index.ts
@@ -1,4 +1,9 @@
-export { Partitions } from "./core"
+export { Partitions, InventoryItemStatus } from "./core"
 export type { DbItem } from "./core"
 export type { UserDetails, UserItem } from "./user"
-export type { CartDetails, CartItem } from "./shopping"
+export type {
+  CartDetails,
+  CartItem,
+  InventoryItemDetails,
+  InventoryItem,
+} from "./shopping"
