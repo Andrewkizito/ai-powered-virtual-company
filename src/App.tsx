7beeaea50900d7f9a3f9ae075373b411c42c6e6b
andyclaudry <andrewkizito54@proton.me>
2026-07-10 09:38:56 +0300

    feat: add inventory and add-inventory routes to the application

--- src/App.tsx
+++ src/App.tsx
@@ -8,17 +8,19 @@ import DashboardLayout from "./components/layout/dashboard/main"
 // Pages
 import AuthGuard from "./features/ui/auth/auth-guard"
 import Dashboard from "./features/ui/admin/Dashboard"
+import Inventory from "./features/ui/admin/inventory/Inventory"
+import AddInventory from "./features/ui/admin/inventory/AddInventory"
 
 export default function App() {
   const auth = useAuth()
-
-  console.log({ auth })
 
   return (
     <Routes>
       {auth.isAuthenticated ? (
         <Route element={<DashboardLayout />}>
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/inventory" element={<Inventory />} />
+          <Route path="/inventory/add" element={<AddInventory />} />
           <Route
             path="*"
             element={<Navigate to="/dashboard" replace={true} />}
